b59902884f82839646b1dc9bfdcd88b759a1061a
Jules Copeland <Jules.Copeland@thetrainline.com>
2018-08-15 21:03:36 +0100

    changed to the lowercase version

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ node default {
 
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README':
+  file { '/root/readme':
     ensure => file,
     content => "Welcome to: ${fqdn}\n",
   }
